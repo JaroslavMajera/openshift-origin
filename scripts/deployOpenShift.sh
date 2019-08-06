bb48403399d6baca7b6aa7db8f1bfa701d77366b
Jaroslav Majera <j.majera@gmc.net>
2019-08-06 13:44:31 +0200

    Refactor elastic memory limit

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -204,7 +204,7 @@ openshift_metrics_heapster_nodeselector={"node-role.kubernetes.io/infra":"true"}
 # Setup logging
 openshift_logging_install_logging=false
 # openshift_logging_es_pvc_dynamic=true
-openshift_logging_es_memory_limit=10Gi
+openshift_logging_es_memory_limit=3500Mi
 openshift_logging_es_pvc_storage_class_name=generic
 openshift_logging_fluentd_nodeselector={"logging":"true"}
 openshift_logging_es_nodeselector={"node-role.kubernetes.io/infra":"true"}
