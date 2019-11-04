b100bf938a545d4048abc2afaba7ef275fd2b236
Jaroslav Majera <j.majera@gmc.net>
2019-11-04 09:30:44 +0100

    Revert timeout

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -55,8 +55,6 @@ sed -i -e "s/^#host_key_checking = False/host_key_checking = False/" /etc/ansibl
 sed -i -e "s/^#pty=False/pty=False/" /etc/ansible/ansible.cfg
 sed -i -e "s/^#stdout_callback = skippy/stdout_callback = skippy/" /etc/ansible/ansible.cfg
 
-sed -i -e "s/^#timeout = 10/timeout = 40/" /etc/ansible/ansible.cfg
-
 # Cloning Ansible playbook repository
 if [ -d /home/${SUDOUSER}/openshift-container-platform-playbooks ]; then
 	echo $(date) "- Pulling playbook"
